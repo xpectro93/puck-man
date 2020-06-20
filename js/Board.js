1a21d438a109f2677aad39911edbc7110d633aa2
xpextro93 <jonathanandrade@pursuit.org>
2020-06-20 02:42:11 -0400

    add puck-man /w movement w/o collision

--- js/Board.js
+++ js/Board.js
@@ -4,8 +4,44 @@ class Board {
         //level should be an array
         this.grid = level;
     }
-    createBoard () {
-    
+    update () {
+
+    }
+    getPlayer
+    render (board) {
+        this.grid.forEach((row,i) => {
+
+            row.forEach((segment, j) => {
+                const box = document.createElement("div");
+
+                box.style.gridRowStart = i +1;
+                box.style.gridColumnStart = j + 1;
+                switch(segment) {
+                    case 0:
+                      box.classList.add("orb");
+                      break;
+                    case 1:
+                        box.classList.add("wall");
+                      break;
+                    case 2:
+                        box.classList.add("ghost-lair");
+                     break;
+                    case 3:
+                           box.classList.add("power-orb")
+                      break;
+                    case 4:
+                        break;
+                    default:
+                      
+                }
+                //   if(i === 490){
+                //       box.classList.add("puck-man");
+                //   }
+                  board.appendChild(box);
+  
+            })
+                
+        })
     }
     
 
--- js/Player.js
+++ js/Player.js
@@ -2,7 +2,7 @@ import Input from './Input.js'
 class Player {
     constructor() {
         //initial index for pucman
-        this.speed  = 1;
+        this.speed  = 3;
         this.coords = { x :10, y:10}
         this.isAlive = true;
         this.input = new Input();
@@ -11,6 +11,7 @@ class Player {
     update () {
         const inputDirection = this.input.getDirection();
         console.log(inputDirection)
+        console.log('this is');
         this.coords.x += inputDirection.x;
         this.coords.y += inputDirection.y;
 
@@ -18,12 +19,23 @@ class Player {
     }
 
     render (board) {
-        // console.log('render player')
-        const puck_man =  document.createElement("div");
+        let child = board.childNodes;
+
+        let puck_man = document.createElement("div");
         puck_man.style.gridRowStart = this.coords.y;
         puck_man.style.gridColumnStart = this.coords.x;
         puck_man.classList.add("puck-man");
-        board.appendChild(puck_man);
+
+        child.forEach(node => {
+            if(node.style.gridRowStart == this.coords.y &&
+               node.style.gridColumnStart ==  this.coords.x){
+                board.replaceChild(puck_man, node)
+                console.log(node.style.gridRowStart, node.style.gridColumnStart)
+            }
+        })
+        
+        // board.appendChild(puck_man);
+        // board[this.coords.x][this.coords.y] = 
         
     }
 
